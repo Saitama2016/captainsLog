ab31bfa2475b8d70f73e2be8b99065bbfce08d41
Vincent Garcia <24626196+Saitama2016@users.noreply.github.com>
2018-08-01 12:48:51 -0700

    Memory Update fixed, now to work on Vacations Edit and Delete

--- public/js/memories.js
+++ public/js/memories.js
@@ -1,15 +1,4 @@
 
-//Check if vacationId exists (does not equal null)
-//Look at how to get vacationId from local storage
-
-// function checkVacationId (vacationId) {
-//     if (vacationId !== null) {
-//         getVacationInputs();
-//     } else {
-//         $('.startMemo').html('<p>No Memories, life is too short, go out and explore!</p>')
-//     }
-// }
-
 const vacationId = localStorage.getItem('vacId');
 
 let listOfMemories;
@@ -42,7 +31,7 @@ function getVacationInputs() {
 }
 
 function postMemory(){
-    $('.memoInput').on('submit', function(event) {
+    $('.newMemoInput').on('submit', function(event) {
         event.preventDefault();
         let memoryDate = $('.memoDate').val();
         let memoryEvent = $('.memoEvent').val();
@@ -115,7 +104,7 @@ function vacationHTML(obj) {
     const vacFlight = obj.flight;
     const vacDeparture = obj.departure;
     return ` 
-    form role="form" class="vacationFileInput modal-content">
+    <form role="form" class="vacationFileInput modal-content">
         <fieldset class="row">
             <div class="closeButton"><i class="fas fa-times fa-3x"></i></div>
             <h3>Vacation</h3>
@@ -196,36 +185,39 @@ $('.memoryList').on('click', '.editMemo', function() {
     let date = $(this).closest('.fullMemo').find('.memoryDate').text();
     let event = $(this).closest('.fullMemo').find('.memoryEvent').text();
     let description = $(this).closest('.fullMemo').find('.memoryDescription').text();
+    console.log(editItemId);
+    console.log(date);
+    console.log(event);
+    console.log(description);
     $('.memoEdit').fadeIn();
     $('.memoEdit').html(memoryEditHTML(editItemId, date, event, description));
 });
 
 function memoryEditHTML(id, date, event, description) {
     return `
-    <form role="form" id=${id}" class="memoInput modal-content">
-    <div class="closeButton"><i class="fas fa-times fa-3x"></i></div>
-    <fieldset class="row">
-        <div><i class="fas fa-times fa-3x closeButton"></i></div>
-        <div class="col-3">
-            <label for="date">Date:</label>
-            <br>
-            <input class="memoDateEdit" type="date" name="date" value='${date}'>
-            <br>
-            <label for="event">Event:</label>
-            <br>
-            <input class="memoEventEdit" type="text" name="memories" value='${event}'>
-        </div>
+    <form role="form" id="${id}" class="memoInput modal-content">
+    <button class="closeButton closeVacForm"><i class="fas fa-times fa-3x"></i></button>
+        <fieldset class="row">
+            <div class="col-3">
+                <label for="date">Date:</label>
+                <br>
+                <input class="memoDateEdit" type="date" name="date" value='${date}'>
+                <br>
+                <label for="event">Event:</label>
+                <br>
+                <input class="memoEventEdit" type="text" name="memories" value='${event}'>
+            </div>
 
-        <div class="col-6">
-            <label for="description">Description:</label>
-            <br>
-            <textarea class="memoDesEdit" rows="5" name="description" value='${description}'></textarea>
-        </div>
-        <div class="col-3">
-            <button class="button Posbutton" type="submit">Submit edit</button>
-        </div>
-    </fieldset>
-</form>
+            <div class="col-6">
+                <label for="description">Description:</label>
+                <br>
+                <textarea class="memoDesEdit" row="5" name="description" maxlength="250" placeholder='${description}' required></textarea>
+            </div>
+            <div class="col-3">
+                <button class="button Posbutton" type="submit">Submit Edit</button>
+            </div>
+        </fieldset>
+    </form>
     `
 }
 
@@ -277,93 +269,99 @@ $('.vacEdit').on('submit', function(e) {
     outEmptyModal($(this));
 });
 
-$('.memoEdit').on('click', '.postButton', function(e) {
-    e.preventDefault();
-    let editItemId = $(this).closest('.memoEdit').find('form').attr('id');
-    let date = $(this).closest('.memoEdit').find('.memoDateEdit').val();
-    let event = $(this).closest('.memoEdit').find('.memoEventEdit').val();
-    let des = $(this).closest('.memoEdit').find('.memoDesEdit').val();
-    $.ajax({
-        type: 'PUT',
-        url: `/api/users/memories/${editItemId}`,
-        beforeSend: function(xhr) {
-            if (window.sessionStorage.accessToken) {
-                xhr.setRequestHeader('Authorization', 'Bearer ' + window.sessionStorage.accessToken);
-            }
-        },
-        data: JSON.stringify({
-            'id': editItemId,
-            'event': event,
-            'date': date,
-            'description': des
-        }),
-        dataType: 'json',
-        contentType: 'application/json',
-        error: error => console.log(error)
+function handleMemoEditSubmit() {
+    $('.memoEdit').on('submit', 'form.memoInput', function(e) {
+        e.preventDefault();
+        console.log('What prevent?');
+        let editItemId = $(this).closest('.memoEdit').find('form').attr('id');
+        let date = $(this).closest('.memoEdit').find('.memoDateEdit').val();
+        let event = $(this).closest('.memoEdit').find('.memoEventEdit').val();
+        let des = $(this).closest('.memoEdit').find('.memoDesEdit').val();
+        $.ajax({
+            type: 'PUT',
+            url: `/api/users/memories/${editItemId}`,
+            beforeSend: function(xhr) {
+                if (window.sessionStorage.accessToken) {
+                    xhr.setRequestHeader('Authorization', 'Bearer ' + window.sessionStorage.accessToken);
+                }
+            },
+            data: JSON.stringify({
+                'id': editItemId,
+                'event': event,
+                'date': date,
+                'description': des
+            }),
+            dataType: 'json',
+            contentType: 'application/json',
+            error: error => console.log(error)
+        });
+        outEmptyModal($(this));
+        getAllMemories();
     });
-    outEmptyModal($(this));
-    getAllMemories();
-});
+}
 
-$(document).ready(() => {
-    $('.signOutYes').click(() => {
-        console.log('click!');
-    });
+function handleMemoryPageButtons () {
+    $(document).ready(() => {
+        $('.signOutYes').click(() => {
+            console.log('click!');
+        });
 
-    $('.closeAbout').click(() => {
-        $('.about').fadeOut();
-    });
+        $('.closeAbout').click(() => {
+            $('.about').fadeOut();
+        });
 
-    $('.signOutOpt').click(() => {
-        console.log('click');
-        $('.signout').fadeIn();
-    });
+        $('.signOutOpt').click(() => {
+            console.log('click');
+            $('.signout').fadeIn();
+        });
 
-    $('.aboutOpt').click(() => {
-        console.log('click');
-        $('.about').fadeIn();
-    });
+        $('.aboutOpt').click(() => {
+            console.log('click');
+            $('.about').fadeIn();
+        });
 
-    $('.closeSignOut').click(() => {
-        $('.signout').fadeOut();
-    });
+        $('.closeSignOut').click(() => {
+            $('.signout').fadeOut();
+        });
 
-    $('.signOutNo').click(() => {
-        $('.signout').fadeOut();
-    });
+        $('.signOutNo').click(() => {
+            $('.signout').fadeOut();
+        });
 
-    $('.addMemories').click(() => {
-        $('.memoriesAdd').fadeIn();
-    });
+        $('.addMemories').click(() => {
+            $('.memoriesAdd').fadeIn();
+        });
     
-    $('.memoryList').on('click', '.viewDes', function() {
-        $(this).closest('.fullMemo').find('.memoHidden').toggleClass('hidden');
-        ($(this).text() == '+')? $(this).text('-'): $(this).text('+');
-    });
+        $('.memoryList').on('click', '.viewDes', function() {
+            $(this).closest('.fullMemo').find('.memoHidden').toggleClass('hidden');
+            ($(this).text() == '+')? $(this).text('-'): $(this).text('+');
+        });
     
-    $('.addMemoButton').on('click', function() {
-        $('.memoAdd').fadeIn();
-    });
+        $('.addMemoButton').on('click', function() {
+            $('.memoAdd').fadeIn();
+        });
     
-    $('.modal').on('click', '.closeButton', function() {
-        $(this).closest('.modal').fadeOut();
-    });
+        $('.modal').on('click', '.closeButton', function() {
+            $(this).closest('.modal').fadeOut();
+        });
 
-    $('.closeMemoForm').click(() => {
-        $('.memoriesAdd').fadeOut();
+        $('.closeMemoForm').click(() => {
+            $('.memoriesAdd').fadeOut();
+        });
     });
-
-});
+}
 
 function outEmptyModal(element) {
     element.closest('.modal').fadeOut();
     element.closest('.modal').empty();
 }
 
-function runMemory() {
+function runMemoryPage() {
     getVacationInputs();
     postMemory();
     getAllMemories();
+    handleMemoryPageButtons();
+    handleMemoEditSubmit();
 }
 
-$(runMemory()); 
+$(runMemoryPage()); 
