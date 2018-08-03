9bc4886b4b4b12f5652ea268a27f4631f834ca53
Vincent Garcia <24626196+Saitama2016@users.noreply.github.com>
2018-08-03 16:51:18 -0700

    Update app.js
    Work on design

--- public/js/app.js
+++ public/js/app.js
@@ -1,65 +1,77 @@
-function generateSignUp () {
-    return `<form autocomplete="on">
-        <fieldset>
-            <div class="log"></div>
-            <legend class="loginRegisterTitle"></legend>
-            <label for="username">Username:</label>
-            <input class="usernameSignUp" type="text" name="username" pattern=".{1,}" required title="1 characters minimum" required>
-            <br>
-            <label for="password">Password:</label>
-            <input class="passwordSignUp" type="text" name="password" pattern=".{10, 72}" required title="3 characters minimum" required>
-            <br>
-            <label for="firstName">First Name:</label>
-            <input class="firstnameSignUp" type="text" name="firstName" required>
-            <br>
-            <label for="lastName">Last Name:</label>
-            <input class="lastnameSignUp" type="text" name="lastName" required>
-            <br>
-            <label for="email">Email:</label>
-            <input class="emailSignUp" type="email" name="email" required>
-            <br>
-            <button class="loginButton signingUpNewAccount" type="submit">Submit</button>
-            </fieldset>
-    </form>
-    <a href="#" id="login"><p class="toggleReg">LogIn</p></a>`;
+'use strict'
+
+function logIn() {
+    $('.loginRegister').on('click', '#login', function () {
+        $('.loginRegister').html(logInTemplate());
+        $('.loginRegister').addClass("box-structure");
+    });
 }
 
 function signUp () {
-    $('.loginRegister').on('click', '#signUp', function() {
+    $('.loginRegister').on( 'click', '#signUp', function() {
         console.log('clicked!');
-        $('.loginRegister').html(generateSignUp());
+        $('.loginRegister').html(signUpTemplate());
         $('.loginRegister').addClass("box-structure");
-    })
+    });
 }
 
-function generateLogin() {
-    return `<form autocomplete="on">
-            <fieldset>
-            <div class="log"></div>
-            <legend class="loginRegisterTitle">Glad you came back!</legend>
-            <label for="username">Username:</label>
-            <input class="usernameLogIn" type="text" name="username" required>
-            <br>
-            <label for="password">Password:</label>
-            <input class="passwordLogIn" type="text" name="password" required>
-            <br>
-            <button class="loginButton signingInAcc" type="submit">Submit</button>
-            </fieldset>
+function logInTemplate() {
+    return `<form class="loginForm" autocomplete="on">
+    <fieldset>
+        <div class="loginError"></div>
+        <legend class="loginRegisterTitle">Glad you came back!</legend>
+        <label for="username">Username:</label>
+        <input class="usernameLogIn" type="text" name="username" required>
+        <br>
+        <label for="password">Password:</label>
+        <input class="passwordLogIn" type="password" name="password" required>
+        <br>
+        <button class="loginButton signingInAcc" type="submit">Submit</button>
+    </fieldset>
+</form>
+<a href="#" id="signUp"><p class="toggleReg">Sign Up!</p></a>`;
+}
+
+function signUpTemplate() {
+    return `<form class="signUpForm" autocomplete="on">
+    <fieldset>
+         <div class="loginError"></div>
+         <legend class="loginRegisterTitle">Welcome, aboard!</legend>
+         <label for="username">Username:</label>
+         <input class="usernameSignUp" type="text" name="username" pattern=".{1,}" required title="1 characters minimum" required>
+         <br>
+         <label for="password">Password:</label>
+         <input class="passwordSignUp" type="password" name="password" pattern=".{10, 72}" required title="10 characters minimum" required>
+         <br>
+         <label for="firstName">First Name:</label>
+         <input class="firstnameSignUp" type="text" name="firstName" required>
+         <br>
+         <label for="lastName">Last Name:</label>
+         <input class="lastnameSignUp" type="text" name="lastName" required>
+         <br>
+         <label for="email">Email:</label>
+         <input class="emailSignUp" type="email" name="email" required>
+         <br>
+         <button class="loginButton signingUpNewAccount" type="submit">Submit</button>
+    </fieldset>
     </form>
-    <a href="#" id="signUp"><p class="toggleReg">Sign up</p></a>`;
+    <a href="#" id="login"><p class="toggleReg">Login!</p></a>`;
 }
 
-function logIn () {
-    $('.loginRegister').on('click', '#login', function() {
-        $('.loginRegister').html(generateLogin());
-        $('.loginRegister').addClass("box-structure");
-    })
+function signInAuth() {
+    $('.loginRegister').on('click', '.signingInAcc', function(event) {
+        event.preventDefault();
+        const username = $('.usernameLogIn').val();
+        const password = $('.passwordLogIn').val();
+        console.log(username, password);
+        postAuthLogin(username, password);
+    });
 }
 
 function postAuthLogin(username, password) {
     $.ajax({
         type: "POST",
-        url: 'api/auth/login',
+        url: '/api/auth/login',
         data: JSON.stringify({
             "username": username,
             "password": password
@@ -70,7 +82,7 @@ function postAuthLogin(username, password) {
             console.log(error);
             if(error.responseText === 'Unauthorized'){
                 $('.loginError').text('The username or password you\'ve entered doesn\'t match any account.');
-            }else {
+            } else {
                 $('.loginError').text('Username/password error');
             }
         }
@@ -78,16 +90,7 @@ function postAuthLogin(username, password) {
     .done(function(json) {
         window.sessionStorage.accessToken = json.authToken;
         localStorage.setItem('userId', json.userId);
-        window.location = 'dashboard.html';
-    });
-}
-
-function signInAuth() {
-    $('.loginRegister').on('click', '.signInAcc', function(event) {
-        event.preventDefault();
-        const username = $('.usernameLogIn').val();
-        const password = $('.passwordLogIn').val();
-        postAuthLogin(username, password);
+        window.location = 'vacations.html';
     });
 }
 
@@ -116,13 +119,14 @@ function signUpAuth() {
                 $('.loginError').text(`${error.responseJSON.location}: ${error.responseJSON.message}`);
             }
         })
-        .done(function(json) {
+        .done(function(json){
             postAuthLogin(username, password);
         });
     });
 }
 
-function indexPage(){
+
+function indexPage() {
     logIn();
     signUp();
     signInAuth();
